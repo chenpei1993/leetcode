89b52dedcc17e3294dc7a9b8175d4e782a043770
chenpei1993 <1035788780@qq.com>
2017-10-02 04:29:48 +0200

    Add files via upload

---
+++ leetcode-20.c
@@ -0,0 +1,34 @@
+//主要是用栈
+bool isValid(char* s) {
+    char stack[1000000];
+    int top=-1;
+    int len=0;
+    
+    while(s[len]) {
+        if (s[len]==')') {
+            if (top>=0 && stack[top]=='(')
+                top--;
+            else
+                return false;
+        }
+        else if (s[len]==']') {
+            if (top>=0 && stack[top]=='[')
+                top--;
+            else
+                return false;
+        }
+        else if (s[len]=='}') {
+            if (top>=0 && stack[top]=='{')
+                top--;
+            else
+                return false;
+        }
+        else {
+            top=top+1;
+            stack[top]=s[len];
+        }
+        
+        len++;
+    }
+    return top==-1;          
+}
